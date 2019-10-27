4cd0c0c1fa2c4d8ddc0a8545b9474f56da2953ad
jordansantos <jordansant@gmail.com>
2019-10-27 17:42:09 -0300

    Criação do LikeController que serve para gerenciar os likes do Post;

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
+
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = new express.Router();
 const upload = multer(uploadConfig);
@@ -11,5 +13,7 @@ const upload = multer(uploadConfig);
 });*/
 routes.get('/posts', PostController.index);
 routes.post('/posts', upload.single('image'), PostController.store);
+
+routes.post('/posts/:id/like', LikeController.store);
 
 module.exports = routes;
