ea9040fd0b07c320fa13791c969dcb7f5b0cbb12
jordansantos <jordansant@gmail.com>
2019-10-27 18:31:03 -0300

    Finalização do frontend da aplicação;

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,25 @@
 // mongodb+srv://omnistack:<omnistack>@omnistack7-mnyi4.mongodb.net/test?retryWrites=true&w=majority
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack7-mnyi4.mongodb.net/test?retryWrites=true&w=majority', {
-  useUnifiedTopology: true, useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 app.use(require('./routes'));
-app.listen(3333);
+server.listen(3333);
